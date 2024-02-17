ff15baad843ff37ab1bf55b5f562aeed67356367
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2024-02-18 00:45:27 +1100

    group logging per project

--- ci/update.sh
+++ ci/update.sh
@@ -8,20 +8,24 @@ echo "Updating the CI environment..."
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "$BASE_DIR is the current directory"
 cd $BASE_DIR/..
 
 PROJECTS_DIR=$( realpath "$BASE_DIR"/../projects )
 
-echo "PROJECTS_DIR is $PROJECTS_DIR"
+echo "Looking for projects in $PROJECTS_DIR"
 
 # Update all projects
 for dir in $PROJECTS_DIR/*; do
   if [ -d "$dir" ]; then
-    echo "Updating $dir..."
+
+    echo "::group::Updating $dir..."
+
     # if a file called before.sh exists in the directory, run it
     [ -f "$dir/before.sh" ] && (cd "$dir" && ./before.sh)
     # if a docker-compose.yml file exists, run docker-compose up -d
     [ -f "$dir/docker-compose.yml" ] && (cd "$dir" && docker compose up -d)
+
+    echo "::endgroup::"
+
   fi
 done
