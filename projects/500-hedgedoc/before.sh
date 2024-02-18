f55b772114115fd7b9cb33d2bc8a7138dd7dc239
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2024-02-18 14:54:17 +1100

    add hedgedoc

---
+++ projects/500-hedgedoc/before.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# /opt/p2-network/run/doppler-token-nyx should exist and contain a Doppler token
+[ -f /opt/p2-network/run/doppler-token-nyx ] || (echo "Doppler token does not exist" && exit 1)
+DOPPLER_TOKEN=$(cat /opt/p2-network/run/doppler-token-nyx)
+
+# /opt/p2-network/run should be created with appropriate permissions
+[ -d /opt/p2-network/run ] || (echo "/opt/p2-network/run does not exist" && exit 1)
+
+DOCKER_HEDGEDOC_DB_USER=hedgedoc
+DOCKER_HEDGEDOC_DB_PASSWORD=$(doppler secrets get DOCKER_HEDGEDOC_DB_PASSWORD --token "${DOPPLER_TOKEN}" --plain)
+DOCKER_HEDGEDOC_DB=hedgedoc
+
+echo "POSTGRES_USER=${DOCKER_HEDGEDOC_DB_USER}" > database.env
+echo "POSTGRES_PASSWORD=${DOCKER_HEDGEDOC_DB_PASSWORD}" >> database.env
+echo "POSTGRES_DB=${DOCKER_HEDGEDOC_DB}" >> database.env
+
+echo "PUID=$(id -u)" > hedgedoc.env
+echo "PGID=$(id -g)" >> hedgedoc.env
+echo "TZ=Australia/Melbourne" >> hedgedoc.env
+echo "CMD_DB_URL=postgres://${DOCKER_HEDGEDOC_DB_USER}:${DOCKER_HEDGEDOC_DB_PASSWORD}@database:5432/${DOCKER_HEDGEDOC_DB}" > app.env
+echo "CMD_DOMAIN=hedgedoc.nyx.thepatrick.cloud" >> hedgedoc.env
+echo "CMD_URL_ADDPORT=false" >> hedgedoc.env
+echo "CMD_PROTOCOL_USESSL=false" >> hedgedoc.env
+echo "CMD_PORT=3000" >> hedgedoc.env
+echo "CMD_ALLOW_ORIGIN=['hedgedoc.nyx.thepatrick.cloud']" >> hedgedoc.env
